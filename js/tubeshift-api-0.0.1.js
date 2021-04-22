c55ca48b0a116a1adbd8e992cdc31c294cf064e3
Tyler Riddle <>
2021-04-21 18:25:09 -0700

    tubeshift api changed license to lgpl

--- js/tubeshift-api-0.0.1.js
+++ js/tubeshift-api-0.0.1.js
@@ -1,6 +1,6 @@
 // TubeShift is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
 // TubeShift is distributed in the hope that it will be useful,
@@ -8,7 +8,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
+// You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 function TubeShiftResult(response_in) {
