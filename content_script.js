75fa1bb76203f45ac56f0cbecce6aba3ff6d43a7
Tyler Riddle <>
2021-04-26 21:58:09 -0700

    fade in doesn't work

--- content_script.js
+++ content_script.js
@@ -13,8 +13,6 @@
 
 const keycode_esc = 27;
 
-var tubeshift_overlay;
-
 function TubeShiftTimeout(duration_in, callback_in) {
     if (callback_in == undefined) {
         throw "must specify a callback";
@@ -103,8 +101,6 @@ function TubeShiftTimeout(duration_in, callback_in) {
 }
 
 function TubeShiftOverlayButton() {
-    this.fade_in = 100;
-    this.fade_out = 500;
     this.show_for = 3500;
     this.img_url = tubeshift_browser_get_asset_url('/icons/tubeshift-overlay.png');
     this.stop_timer = undefined;
@@ -136,6 +132,8 @@ function TubeShiftOverlayButton() {
         const p_e = document.createElement('p');
         const img_e = document.createElement('img');
 
+        $(div_e).hide();
+
         p_e.textContent = 'X';
         $(p_e).css("color", "black");
         $(p_e).css("font-size", "15px");
@@ -155,24 +153,18 @@ function TubeShiftOverlayButton() {
         $(img_e).css('cursor', 'pointer');
         $(img_e).hover(this._hover_in, this._hover_out);
 
-        $(div_e).hide();
         div_e.appendChild(p_e);
         div_e.appendChild(img_e);
 
         this.element = div_e;
     };
 
-    this._show = async () => {
-        await new Promise((resolve, reject) => {
-            $(this.element).fadeIn(this.fade_in);
-            resolve();
-        });
+    this._show = function () {
+        $(this.element).show();
     };
 
-    this._hide = async () => {
-        await new Promise((resolve, reject) => {
-            $(this.element).fadeOut(this.fade_out);
-        });
+    this._hide = function() {
+        $(this.element).hide();
     };
 
     this._key_handler = (event) => {
@@ -184,31 +176,31 @@ function TubeShiftOverlayButton() {
         return true;
     };
 
-    this.start = async () => {
+    this.start = function () {
+        if (this.stop_timer != undefined) {
+            console.error("why?");
+            throw "can't start an overlay that is already started";
+        }
+
         this.stop_timer = new TubeShiftTimeout(this.show_for, () => {
             this.stop();
         });
 
         $(document).on('keyup', this._key_handler);
-
-        await new Promise((resolve, reject) => {
-            this._show().then(resolve);
-        });
-
+        this._show();
         return false;
     };
 
-    this.stop = async () => {
+    this.restart = function () {
+        this.stop_timer.reset();
+        this._show();
+    }
+
+    this.stop = function () {
         $(document).off('keyup', this._escape_handler);
 
         this.stop_timer.stop();
-        this.stop_timer = undefined;
-
-        await new Promise((resolve, reject) => {
-            this._hide().then(resolve);
-        });
-
-        this.element.parentNode.removeChild(this.element);
+        this._hide();
 
         return false;
     };
@@ -228,31 +220,38 @@ function TubeShiftOverlayButton() {
     }
 }
 
-function tubeshift_cs_handle_available(count) {
-    const video_element = $("video")[0];
-    const video_container = $(video_element).parent()[0];
+{
+    var tubeshift_overlay;
 
-    if (tubeshift_overlay == undefined) {
-        tubeshift_overlay = new TubeShiftOverlayButton();
+    function tubeshift_cs_handle_available(count) {
+        const video_element = $("video")[0];
+        const video_container = $(video_element).parent()[0];
+
+        if (tubeshift_overlay == undefined) {
+            tubeshift_overlay = new TubeShiftOverlayButton();
+            var overlay_element = tubeshift_overlay.element;
+
+            $(overlay_element).css("position", "absolute");
+            $(overlay_element).css("left", "15px");
+            $(overlay_element).css("top", "15px");
+
+            $(overlay_element).on("click", () => {
+                tubeshift_browser_send_bg_page_message({ name: "shift" });
+                tubeshift_overlay.stop();
+                tubeshift_overlay = undefined;
+                video_element.pause();
+                return false;
+            });
+
+            video_container.appendChild(overlay_element);
+
+            tubeshift_overlay.start();
+        } else {
+            tubeshift_overlay.restart();
+        }
+
+        return;
     }
-
-    const overlay_element = tubeshift_overlay.element;
-
-    $(overlay_element).css("position", "absolute");
-    $(overlay_element).css("left", "15px");
-    $(overlay_element).css("top", "15px");
-
-    $(overlay_element).on("click", () => {
-        tubeshift_overlay.stop();
-        tubeshift_browser_send_bg_page_message({ name: "shift" });
-        video_element.pause();
-        return false;
-    });
-
-    video_container.appendChild(overlay_element);
-
-    tubeshift_overlay.start();
-    return;
 }
 
 function tubeshift_cs_handle_message(message) {
